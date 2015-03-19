70f7ee8c11dd2db9109bc9a4c46efee1622ac6ac
Josh Burgin <jsburgin@crimson.ua.edu>
2015-03-19 16:53:01 -0500

    updated procfile and urls

--- ranker/urls.py
+++ ranker/urls.py
@@ -4,5 +4,4 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^', include('ranks.urls', namespace="ranks")),
     url(r'^admin/', include(admin.site.urls)),
-    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': 'https://dev.burgin.io/favicon.co'}),
 )
